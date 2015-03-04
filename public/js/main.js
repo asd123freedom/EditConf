1e30ff58c17e791643c8646e36046141f889877a
unknown <liuchf@BIH-D-2565.youdao.internal>
2015-03-04 12:56:21 +0800

    20150304

--- public/js/main.js
+++ public/js/main.js
@@ -4,24 +4,45 @@ app.run(["$rootScope", "$location", function($rootScope, $location) {
 }]);
 
 app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
-  /*$routeProvider.when("/login", {
-    templateUrl: "login.html",
-    controller: "LoginCtrl"
-  });*/
+  $routeProvider.when("/config", {
+    templateUrl: "config.html",
+    controller: "ConfigCtrl"
+  });
   return $locationProvider.html5Mode(false);
 }]);
 app.controller("MainCtrl", function($scope, api, $rootScope) {
+    $scope.active = function($event) {
+          
+    }
+});
+app.controller("ConfigCtrl", function($scope, api, $rootScope) {
     var str="";
     api.plainGet("GetJSON", {file_path: str})
     .success(function(data) {
-      var obj = data;
-      console.log(obj);
-      var parent = $(".tree > ul > li.temp");
-      drawTree(obj.struts, parent);
-      $(".tree>ul ul").hide();
+        var obj = data;
+        console.log(obj);
+        var parent = $(".tree > ul > li.temp");
+        drawTree(obj.struts, parent);
+        $(".tree>ul ul").hide();
     });
+    $scope.showChild = function ($event) {
+        var target = $event.target;
+        if(target.nodeName.toLowerCase() == "span") {
+            var $ul = $(target).next();
+            if($ul.length > 0) {
+                if($ul.is(":visible")){
+                    $ul.slideUp();
+                } else {
+                    $ul.slideDown();
+                }
+            }
+        }
+    }
+    function isArray(arr) {
+          return Object.prototype.toString.apply(arr) === "[object Array]";
+    }
     function drawTree(data,node) {
-        //不加这句话item会成全局变量
+        //without this line the itme will be global
         var item;
         for(item in data) {
           //console.log(item);
@@ -48,57 +69,6 @@ app.controller("MainCtrl", function($scope, api, $rootScope) {
           }
         }
     }
-    /*
-    function getXMLJSON(str) {
-        $.ajax({
-            url: "../GetJSON",
-            type: "GET",
-            data: {file_path: str},
-            success:function(data){
-                try
-                {
-                    //var obj=JSON.parse(data.jsonstr);
-                    var obj = data;
-                    console.log(obj);
-                    var parent=$(".tree > ul > li.temp");
-                    //$(".tree > ul > li").find("span").text(obj["@name"]);
-                    drawTree(obj.struts,parent);
-                    $(".tree>ul ul").hide();
-                    //单击时将节点的属性值显示在表格中
-                    $(".tree span").on("click",function(event){
-                        var $this = $(this);
-                        $("table.attr").data("span",$this);
-                        renderTable($this.data("data"));
-                        $("table.edit").data("data",$this);
-                    });
-                    //双击是显示节点的子节点
-                    $(".tree span").on("dblclick",function(event) {
-                        var $this = $(this);
-                        var $ul = $this.parent().children("ul");
-                        if ($ul.length > 0) {
-                            if($ul.is(":visible")){
-                                $ul.slideUp();
-                                //$this.children("i").removeClass("fa-minus").addClass("fa-plus");
-                            } else {
-                                $ul.slideDown();
-                                //$this.children("i").removeClass("fa-plus").addClass("fa-minus");
-                            }
-                        }
-                    });
-                    //点击按钮调用action完成对配置文件的修改工作
-                    $(".editNode").bind("click",function(){
-                        var node = $("table.edit").data("data");
-                        getNodeInfo(node);
-                    });
-                }
-                catch (error){
-                    console.log(error);
-                }                           
-            } 
-        });
-    } 
-    */       
-
 });
 app.factory("api", ["$http", "$q", function($http, $q){
     var _url, api, plainRequest;
